9c4a70c5afb565e14c3b8c6e07bb0192fa4b163b
gabi <gabriellabenevides22@gmail.com>
2026-03-25 22:07:38 -0300

    Adiciona projeto de testes unitários para ProvisaoDevolucao
    Inclui o projeto `FinancialProvision.UnitTests` na solução `FinancialProvision.API.sln` com configurações de build. O arquivo `FinancialProvision.UnitTests.csproj` foi criado para o .NET 9.0, com referências a pacotes de teste. Foram implementados testes unitários para a classe `ProvisaoDevolucao`, cobrindo criação, atualização e utilização de valores, além da verificação da data de criação.

---
+++ FinancialProvision.API/FinancialProvision.UnitTests/Domain/Entities/ProvisaoDevolucaoTests.cs
@@ -0,0 +1,65 @@
+﻿using FinancialProvision.Provision.Domain.Entities;
+
+namespace FinancialProvision.UnitTests.Domain.Entities;
+
+public class ProvisaoDevolucaoTests
+{
+    [Fact]
+    public void Deve_Criar_Provisao_Com_Valores_Corretos()
+    {
+        // Arrange
+        var mes = 1;
+        var ano = 2026;
+        var valorPrevisto = 1000;
+        var descricao = "teste";
+
+        // Act
+        var provisao = new ProvisaoDevolucao(mes, ano, valorPrevisto, descricao);
+
+        // Assert
+        Assert.Equal(mes, provisao.Mes);
+        Assert.Equal(ano, provisao.Ano);
+        Assert.Equal(valorPrevisto, provisao.ValorPrevisto);
+        Assert.Equal(0, provisao.ValorUtilizado);
+        Assert.Equal(descricao, provisao.Descricao);
+        Assert.NotEqual(default, provisao.DataCriacao);
+    }
+
+    [Fact]
+    public void Deve_Atualizar_Valor_Previsto()
+    {
+        // Arrange
+        var provisao = new ProvisaoDevolucao(1, 2026, 1000, "teste");
+
+        // Act
+        provisao.AtualizarValor(2000, "novo valor");
+
+        // Assert
+        Assert.Equal(2000, provisao.ValorPrevisto);
+        Assert.Equal("novo valor", provisao.Descricao);
+    }
+
+    [Fact]
+    public void Deve_Somar_Valor_Utilizado()
+    {
+        // Arrange
+        var provisao = new ProvisaoDevolucao(1, 2026, 1000, "teste");
+
+        // Act
+        provisao.RegistrarUtilizacao(100);
+        provisao.RegistrarUtilizacao(50);
+
+        // Assert
+        Assert.Equal(150, provisao.ValorUtilizado);
+    }
+
+    [Fact]
+    public void DataCriacao_Deve_Ser_Preenchida_Automaticamente()
+    {
+        // Arrange & Act
+        var provisao = new ProvisaoDevolucao(1, 2026, 1000, "teste");
+
+        // Assert
+        Assert.True(provisao.DataCriacao <= DateTime.UtcNow);
+    }
+}
